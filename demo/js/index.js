03aa2b88be6e048df8c43c8368240b7b36ed4603
06wj <06wj@163.com>
2018-11-27 20:05:05 +0800

    rm a

--- demo/js/index.js
+++ demo/js/index.js
@@ -2,11 +2,10 @@ var containerElem = document.querySelector('#listContainer');
 models.forEach(function(modelInfo){
     var name = modelInfo.name;
     var id = modelInfo.id;
-    var elem = document.createElement('a');
+    var elem = document.createElement('div');
     var modelPath = `../models/${id}/`;
     elem.className = 'modelContainer';
-    elem.href = `https://hiloteam.github.io/Hilo3d/examples/glTFViewer/index.html?url=https://06wj.github.io/pokemon/models/${id}/glTF/model.gltf`;
-    elem.target = '_blank';
+    elem.setAttribute('data-id', id);
     elem.innerHTML = `
         <img src="./${modelPath}icon.png" width="74" height="63" />
         <div class='modelInfo'>${id} ${name}</div>
